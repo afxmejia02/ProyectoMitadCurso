d81ca29a96907d8bfcb582b2cc0f8830f052691c
afxmejia02 <130402869+afxmejia02@users.noreply.github.com>
2023-07-13 16:14:41 -0500

    Update urls.py
    Cambio path('editar/persona/<int:ciudad_id>/', views.editar_persona, name='editar_persona'), por path('editar/persona/<int:persona_id>/', views.editar_persona, name='editar_persona')
    
    Error corregido

--- Modulos/Index/urls.py
+++ Modulos/Index/urls.py
@@ -8,6 +8,6 @@ urlpatterns = [
     path('crear',views.crear, name='crear'),
     path('eliminar/<int:Id>', views.eliminar_persona, name='eliminar_persona'),
     path('eliminar/ciudad/<int:Id>/', views.eliminar_ciudad, name='eliminar_ciudad'),
-    path('editar/persona/<int:ciudad_id>/', views.editar_persona, name='editar_persona'),
+    path('editar/persona/<int:persona_id>/', views.editar_persona, name='editar_persona'),
     path('editar/ciudad/<int:ciudad_id>/', views.editar_ciudad, name='editar_ciudad'),
 ]
